8144dcef841095b518d971f51cd4317351550591
gregoire charles <gcm.charles@gmail.com>
2017-01-12 23:28:56 +0100

    remove all attempts to deploy to heroku :)

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
 require 'sinatra'
 
-# NOTE: ENV variables should be set directly in terminal for testing on localhost
-
-# Talk to Facebook
 get '/webhook' do
   params['hub.challenge'] if ENV["VERIFY_TOKEN"] == params['hub.verify_token']
 end
